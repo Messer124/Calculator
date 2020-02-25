e40b72ec708dd3e86501a831cde42be41fab09a3
GDmitriy <Messer124@github.com>
2020-02-25 20:11:46 +0300

    -Заменил классы для проверки чисел на целостность, величину и проверку на одинаковую числительную систему одним регулярным выражением. -Результат вычислений выдается в той же числительной системе в какой были входные числа.

--- src/Calc.java
+++ src/Calc.java
@@ -8,20 +8,28 @@ public class Calc {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input = reader.readLine();
         String parts[] = input.split(" ");
+        String regex = "(([1][0]|[1-9]) (\\+|\\-|\\*|\\/) ([1][0]|[1-9]))|((I?X|I{1,3}|I?VI{0,3}) (\\+|\\-|\\*|\\/) (I?X|I{1,3}|I?VI{0,3}))";
+        boolean correctString = input.matches(regex);
+        boolean isRome = false;
+        int result = 0;
 
-        if(AllConditionsRight.allConditionsRight(parts[0], parts[2])){
+        if(correctString){
             if (IsArabic.isArabic(parts[0], parts[2])) {
                 number1 = Integer.parseInt(parts[0]);
                 number2 = Integer.parseInt(parts[2]);
             } else {
                 number1 = FromRomeToArab.fromRomeToArab(parts[0]);
                 number2 = FromRomeToArab.fromRomeToArab(parts[2]);
+                isRome = true;
             }
+            if (parts[1].equals("+"))      result = number1+number2;
+            else if (parts[1].equals("-")) result = number1-number2;
+            else if (parts[1].equals("*")) result = number1*number2;
+            else if (parts[1].equals("/")) result = number1/number2;
 
-            if (parts[1].equals("+")) System.out.println(number1+number2);
-            else if (parts[1].equals("-")) System.out.println(number1-number2);
-            else if (parts[1].equals("*")) System.out.println(number1*number2);
-            else if (parts[1].equals("/")) System.out.println(number1/number2);
+            if (isRome) {
+                System.out.println(GetIntegerFromRoman.getInteger(result));
+            } else System.out.println(result);
         } else throw new Exception();
     }
 }
--- src/FromRomeToArab.java
+++ src/FromRomeToArab.java
@@ -1,7 +1,10 @@
 public class FromRomeToArab {
     public static int fromRomeToArab(String numb) {
-        String[] arab = new String[]{"10","1","2","3","4","5","6","7","8","9"};
-        String[] rome = new String[]{"X","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String[] arab = new String[100];
+        for (int i = 0; i < arab.length; i++) {
+            arab[i] = String.valueOf(i+1);
+        }
+        String[] rome = new String[]{"I","II","III","IV","V","VI","VII","VIII","IX","X"};
 
         int position = 0;
         for (int i = 0; i < rome.length; i++) {
